8dfbcb80e34a5a2fe2089270de1a91b3b5196284
SNIKHIL KUMAR <133903280+SNIKHILKUMAR@users.noreply.github.com>
2026-01-07 11:17:50 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+// const arr1= [1,2,3,4]
+// const arr2= ["baby","boy",]
+
+// arr3= arr1.concat(arr2)    //concat method to join two array 
+// console.log(arr3)
+
+// arr4=[...arr1, ...arr2 ]        //spread method to join two array 
+// console.log(arr4)
+
+// const arr5=[1,2,3,[3,4],5,[4,6,[5,8]]]   // bhot sare array ek sath solve karna 
+// arr6= arr5.flat(Infinity)
+// console.log(arr6)
+
+
+console.log(Array.isArray("Nikhil"))
+console.log(Array.from("NIKHIL"))
+console.log(Array.from({name: "NIKHIl"})) // interresting 
+
+const arr7 =700
+const arr8= 800
+const arr9 = 900 
+console.log( Array.of(arr7,arr8,arr9))
