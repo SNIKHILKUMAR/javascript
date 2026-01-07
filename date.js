68421dafce2d35ba3c56a9eb275ac87f671784c6
SNIKHIL KUMAR <133903280+SNIKHILKUMAR@users.noreply.github.com>
2026-01-07 11:18:46 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,16 @@
+// let timeStamp = new Date()
+//     console.log(timeStamp.toString())
+//     console.log(timeStamp.toDateString())
+//     console.log(timeStamp.toLocaleString())
+    
+//     console.log(`${timeStamp.getMonth()+1 } and the day is `)
+
+const  arr1= [1,2,4,5,6]
+ arr1.push(7)
+ arr1.pop(1)
+ arr1.unshift(89)
+ arr1.shift()
+console.log(arr1)
+
+
+
