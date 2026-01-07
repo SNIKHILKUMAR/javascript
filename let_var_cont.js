012300b4867f1113f74e6100dee9e2be71ccf08a
SNIKHIL KUMAR <133903280+SNIKHILKUMAR@users.noreply.github.com>
2026-01-07 11:19:01 +0530

    Add files via upload

---
+++ let_var_cont.js
@@ -0,0 +1,14 @@
+const variable=43;
+let account="spideman";
+var activa="superman";
+
+
+account="batman";
+activa ="wonderwoman";
+abc="hero";
+let xyz;
+/*
+var not prefferd
+because it create issue in block scope and function scope 
+*/ 
+console.log([variable, account, activa, abc, xyz]);
