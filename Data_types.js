4da6dc193d30f6703527f5013b1c459246669409
SNIKHIL KUMAR <133903280+SNIKHILKUMAR@users.noreply.github.com>
2026-01-07 11:18:33 +0530

    Add files via upload

---
+++ Data_types.js
@@ -0,0 +1,71 @@
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null > 0)
+
+
+
+// const name= "Nikhil";
+// const repo= 45;
+
+// console.log(`Heloo my name is ${name} and my repo is ${repo}`)
+
+// const repoName= new String("helloSir")
+// console.log(repoName[0])
+// console.log(repoName.length)
+// console.log(repoName.toUpperCase())
+// console.log(repoName.indexOf('o'))
+// console.log(repoName.charAt(3))
+
+// const student= repoName.substring(0,4)
+// console.log(student)
+
+// const g_name="   abcdefgh     b"
+// console.log(g_name)
+// console.log(g_name.trim())
+
+// const contentString = "Hello, world";
+
+// document.body.innerHTML = contentString.big();
+// console.log(contentString.blink());
+// console.log(contentString.big());
+// console.log(contentString.bold());
+
+
+
+
+// const content1String = "Hello, world";
+// const elem = document.createElement("b");
+
+
+// // elem.innerText = content1String;
+// // document.body.appendChild(elem);
+// //  console.log(content1String.bold())
+
+//  const sentence = "The quick brown fox jumps over the lazy dog.";
+
+// const index = 4;
+
+// console.log(`Character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt( index)}`);
+// console.log(sentence.length)
+
+
+// const icons = "☃★♲";
+
+// console.log(icons.codePointAt(1));
+// // Expected output: "9733"
+
+// const arr1= ["1","2"];
+// const arr2= ["3","4"];
+// console.log(arr1.concat(arr2));
+
+
+const sum =89
+console.log(sum)
+
+const sum1= new Number(1000000)
+console.log(sum1)
+console.log(sum1.toLocaleString( 'en-IN'))
+
+
+console.log(Math)
+console.log(Math.round(4.6))
